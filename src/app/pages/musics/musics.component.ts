d655e7487144e205d755214c931cd4baf252edb3
Fabio Leite <fabioleite.rio@gmail.com>
2019-11-01 06:36:25 -0300

    1

--- src/app/pages/musics/musics.component.ts
+++ src/app/pages/musics/musics.component.ts
@@ -11,12 +11,38 @@ import { DataService } from 'src/app/services/data.service';
 export class MusicsComponent implements OnInit {
 
   public musics$: Observable<RespMusic> = null;
+  public musics: Music[] = null;
+  public msg : string;
+  public sucess: boolean;
 
   constructor(private dataservice: DataService) { }
 
   ngOnInit() {
     this.musics$ = this.dataservice.getMusics();
+   
 
+    this.musics$.subscribe(
+      res =>  this.sucess = res.success
+    );
+    this.musics$.subscribe(
+      res =>  this.musics = res.data
+    );
+    if(this.msg == ""){
+      this.msg = "vazio";
+    }
+    if(sucess)
+    {
+     
+     // this.musics = [];
+
+      this.musics.push(new  Music("0001","001","Musica1","pop"));
+      this.musics.push(new  Music("0002","002","Musica2","samba"));
+    }
+     else
+     {
+      
+    }
+  
   }
 
 
